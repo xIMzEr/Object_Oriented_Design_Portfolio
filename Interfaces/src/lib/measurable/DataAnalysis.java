bba37fd830442fc13d0e99918cc7bdf382f54aee
sbarn <sbarn@10.34.6.66>
2019-11-20 11:58:43 +0000

    " "

--- Interfaces/src/lib/measurable/DataAnalysis.java
+++ Interfaces/src/lib/measurable/DataAnalysis.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class DataAnalysis<E extends Measurable, Measurable> {
+public class DataAnalysis<E extends Measurable> {
 
 	private List<E> objects;
 
